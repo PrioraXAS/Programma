1571291a9cd15ac8bb703c7a50653854489edf6b
PrioraXAS <>
2023-10-25 09:31:50 +0400

    ver1

---
+++ МДК лучшее приложение в мире/The best programm/The best programm/FormCr.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace The_best_programm
+{
+    partial class FormCr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonSend = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonSend
+            // 
+            this.ButtonSend.BackColor = System.Drawing.Color.RosyBrown;
+            this.ButtonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ButtonSend.Location = new System.Drawing.Point(14, 229);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(186, 45);
+            this.ButtonSend.TabIndex = 6;
+            this.ButtonSend.Text = "Оформить заказ";
+            this.ButtonSend.UseVisualStyleBackColor = false;
+            this.ButtonSend.Click += new System.EventHandler(this.ButtonSend_Click_1);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.BackColor = System.Drawing.Color.RosyBrown;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Цезарь",
+            "Курица",
+            "Стейк",
+            "Пицца",
+            "Бургер",
+            "Паста",
+            "Чай",
+            "Кофе",
+            "Лимонад"});
+            this.checkedListBox2.Location = new System.Drawing.Point(14, 26);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(145, 169);
+            this.checkedListBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RosyBrown;
+            this.button1.Location = new System.Drawing.Point(165, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormCr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(218, 302);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.ButtonSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormCr";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormCr";
+            this.Load += new System.EventHandler(this.FormCr_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ButtonSend;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ МДК лучшее приложение в мире/The best programm/The best programm/FormRegistration.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace The_best_programm
+{
+    partial class FormRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRegistration));
+            this.TextBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.ButtonRegistration = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TextBoxSurname
+            // 
+            this.TextBoxSurname.BackColor = System.Drawing.Color.RosyBrown;
+            this.TextBoxSurname.Font = new System.Drawing.Font("Arial", 14F);
+            this.TextBoxSurname.Location = new System.Drawing.Point(39, 209);
+            this.TextBoxSurname.MaxLength = 13;
+            this.TextBoxSurname.Multiline = true;
+            this.TextBoxSurname.Name = "TextBoxSurname";
+            this.TextBoxSurname.Size = new System.Drawing.Size(217, 36);
+            this.TextBoxSurname.TabIndex = 42;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.BackColor = System.Drawing.Color.RosyBrown;
+            this.textBoxName.Font = new System.Drawing.Font("Arial", 14F);
+            this.textBoxName.Location = new System.Drawing.Point(39, 94);
+            this.textBoxName.MaxLength = 13;
+            this.textBoxName.Multiline = true;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(217, 36);
+            this.textBoxName.TabIndex = 43;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.Color.RosyBrown;
+            this.textBoxPassword.Font = new System.Drawing.Font("Arial", 14F);
+            this.textBoxPassword.Location = new System.Drawing.Point(548, 260);
+            this.textBoxPassword.MaxLength = 13;
+            this.textBoxPassword.Multiline = true;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(217, 36);
+            this.textBoxPassword.TabIndex = 44;
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.BackColor = System.Drawing.Color.RosyBrown;
+            this.textBoxNumber.Font = new System.Drawing.Font("Arial", 14F);
+            this.textBoxNumber.Location = new System.Drawing.Point(39, 325);
+            this.textBoxNumber.MaxLength = 13;
+            this.textBoxNumber.Multiline = true;
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(217, 36);
+            this.textBoxNumber.TabIndex = 45;
+            this.textBoxNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxNumber_KeyPress);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.BackColor = System.Drawing.Color.RosyBrown;
+            this.textBoxLogin.Font = new System.Drawing.Font("Arial", 14F);
+            this.textBoxLogin.Location = new System.Drawing.Point(548, 144);
+            this.textBoxLogin.MaxLength = 13;
+            this.textBoxLogin.Multiline = true;
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(217, 36);
+            this.textBoxLogin.TabIndex = 46;
+            // 
+            // ButtonRegistration
+            // 
+            this.ButtonRegistration.BackColor = System.Drawing.Color.RosyBrown;
+            this.ButtonRegistration.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonRegistration.ForeColor = System.Drawing.Color.Black;
+            this.ButtonRegistration.Location = new System.Drawing.Point(265, 387);
+            this.ButtonRegistration.Name = "ButtonRegistration";
+            this.ButtonRegistration.Size = new System.Drawing.Size(290, 51);
+            this.ButtonRegistration.TabIndex = 48;
+            this.ButtonRegistration.Text = "Зарегистрироваться";
+            this.ButtonRegistration.UseVisualStyleBackColor = false;
+            this.ButtonRegistration.Click += new System.EventHandler(this.ButtonRegistration_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(316, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 37);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Регистрация";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(44, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 50;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(44, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(44, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 20);
+            this.label4.TabIndex = 52;
+            this.label4.Text = "Номер телефона";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(554, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 20);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "Логин";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(554, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 20);
+            this.label6.TabIndex = 54;
+            this.label6.Text = "Пароль";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(277, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 267);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 55;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RosyBrown;
+            this.button1.Location = new System.Drawing.Point(767, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 33);
+            this.button1.TabIndex = 56;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonRegistration);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.TextBoxSurname);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormRegistration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Регистрация";
+            this.Load += new System.EventHandler(this.FormRegistration_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxSurname;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Button ButtonRegistration;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
